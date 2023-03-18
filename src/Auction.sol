61b406a6d02af83ecfcf1cf6a0795f851867622b
dadsec-dev <secdad1@gmail.com>
2023-03-18 22:59:35 +0100

    updated changes

--- src/Auction.sol
+++ src/Auction.sol
@@ -1,73 +1,76 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
-
-
-contract BlindAuction {
-
-// Structure to hold information about a bid
-struct Bid {
-    address payable bidder; // Ethereum address of the bidder
-    uint256 amount; // Amount of ether bid
-}
-
-address payable public owner; // Ethereum address of the contract owner
-IERC721 public nft; // ERC721 token being auctioned
-uint256 public startBlock; // Block number when auction starts
-uint256 public endBlock; // Block number when auction ends
-
-Bid[] public bids; // Array to hold all the bids
-mapping(address => bytes32) public blindedBids; // Mapping to hold blinded bids
-
-// Constructor function to set initial values
-constructor(IERC721 _nft, uint256 _startBlock, uint256 _endBlock) {
-    owner = payable(msg.sender);
-    nft = _nft;
-    startBlock = _startBlock;
-    endBlock = _endBlock;
-}
-
-// Function to place a blinded bid
-function placeBid(bytes32 _blindedBid) public payable {
-    require(block.number >= startBlock && block.number <= endBlock, "Auction is not active");
-    require(msg.value > 0, "Bid amount must be greater than zero");
-    require(blindedBids[msg.sender] == 0, "You have already placed a bid");
-
-    blindedBids[msg.sender] = _blindedBid;
-    bids.push(Bid(payable(msg.sender), msg.value));
-}
-
-// Function to reveal a blinded bid
-function revealBid(uint256 _value, bytes32 _secret) public {
-    require(block.number > endBlock, "Auction is still active");
-    require(blindedBids[msg.sender] != 0, "You have not placed a bid");
-    require(_value == bids[bids.length - 1].amount, "Invalid value");
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+import "../lib/openzeppelin-contracts/contracts/token/ERC721/IERC721.sol";
+contract Auction {
     
-    bytes32 hash = keccak256(abi.encodePacked(_value, _secret));
-    require(hash == blindedBids[msg.sender], "Invalid secret");
-
-    bids[bids.length - 1].bidder.transfer(_value);
-    nft.transferFrom(address(this), bids[bids.length - 1].bidder, nft.tokenOfOwnerByIndex(owner, 0));
-}
-
-// Function to end the auction and transfer the NFT to the highest bidder
-function endAuction() public {
-    require(block.number > endBlock, "Auction is still active");
-    require(msg.sender == owner, "Only the owner can end the auction");
+   address public owner;
     
-    uint256 highestBid = 0;
-    address payable highestBidder;
+    struct Auction {
+        uint128 startBlock;
+        uint128 endBlock;
+        address  auctionCreator;
+        address auctionedItem;
+        address highestBidder;
+        uint highestBid;
+        uint itemID;
+        bool started;
+        
+    }
+uint id;
+mapping(uint => Auction) public auctionID;
+mapping(address =>mapping( uint => uint)) public userBidAMount;
 
-    for (uint256 i = 0; i < bids.length; i++) {
-        if (bids[i].amount > highestBid) {
-            highestBid = bids[i].amount;
-            highestBidder = bids[i].bidder;
-        }
+    error invalidBlock();
+    error  notStarted();
+    constructor() {
+        owner = msg.sender;
     }
 
-    nft.transferFrom(address(this), highestBidder, nft.tokenOfOwnerByIndex(owner, 0));
-    owner.transfer(address(this).balance);
-}
 
+    function createAuction(uint128 _startBlock, uint128 _endBlock, address _auctionItem, uint256 _nftID)external{
+        require(_auctionItem != address(0));
+
+        if(_endBlock<= _startBlock) revert invalidBlock();
+        Auction storage auction = auctionID[id];
+        auction.startBlock = _startBlock;
+        auction.endBlock = _endBlock;
+        auction.auctionCreator = msg.sender;
+        auction.auctionedItem = _auctionItem;
+        auction.started = true;
+        auction.itemID = _nftID;
+        IERC721(_auctionItem).transferFrom(msg.sender, address(this), _nftID);
+        id++; 
+
+
+    }
+    function bid(uint256 _auctionID) external payable {
+        require(msg.value != 0, "empty value");
+        require(msg.sender != owner, "Ole");
+        
+        Auction storage auction =  auctionID[_auctionID];
+       if(auction.started) revert notStarted();
+       require(msg.sender != auction.auctionCreator);
+       userBidAMount[msg.sender][_auctionID] = msg.value;
+        if(msg.value > auction.highestBid) {
+            auction.highestBid = msg.value;
+            auction.highestBidder = msg.sender;
+       }
+       
+        
+    }
+    // function updatebBid() external payable {
+    //     userBid[msg.sender] += msg.value;
+    // }
+
+
+
+
+
+
+
+
+
+    function isOwner() internal view {
+        require(msg.sender == owner);
+    }
 }
